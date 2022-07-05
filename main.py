46605e1ea6947a5bad78c31c43420776ebb581b2
bikesh <c0859472@mylambton.ca>
2022-07-05 12:58:56 -0400

    Create new program file with task division

---
+++ main.py
@@ -0,0 +1,42 @@
+# ***************************************************************************
+
+# This is program to view file, add, delete and update the file as per user requirement.
+
+# *****************************************************************************
+
+# This function is to view the file from txt file.
+def view():
+    print("x")
+
+# Ritika
+# This function is to add the name into existing txt file
+def addition():
+    print("x")
+
+
+# Hemanta and Rosy
+# This function is to delete the name from existing txt file
+def delete():
+    print("x")
+
+
+# padam and bikesh
+# This function is to update the file to existing txt file
+def update():
+    print("x")
+
+
+
+# this function is to exit the program.
+def exit():
+    print("x")
+
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    view()
+    addition()
+    delete()
+    update()
+    exit()
